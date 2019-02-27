f2c5e820efbdf9a48bc84e67a96c5146d8dbb267
Deniz Ezgi Kurt <denizezgikurt@gmail.com>
2019-02-27 12:59:56 +0100

    initial commit for Glorious Assignment Project

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,19 @@
-import '../styles/index.scss';
+var images = document.getElementsByClassName('image-wrapper');
 
-var button = document.getElementById('button');
-var greencolor = '#77f442';
-var pinkcolor = '#ba4181';
-var bluecolor = '#3a65af';
-var redcolor = '#961029';
-
-button.onclick= function () {
-
-var randomNumber = Math.floor(Math.random()*10);
-
-if(randomNumber % 2 == 0) {
-    document.body.style.backgroundColor = greencolor;
+for(var i = 0; i < images.length; i++) {
+    var image = images[i];
+    image.onclick = function() {
+        if(this.className.indexOf('selected') > 0) {
+            this.className = 'image-wrapper';
+        }
+        else {
+            this.className = 'image-wrapper' + ' selected';
+        }
+    };
 }
-else if(randomNumber %3 ==0) {
-    document.body.style.backgroundColor = pinkcolor;
-}
-else if(randomNumber %5 == 0) {
-    document.body.style.backgroundColor = bluecolor;
-}
-else {
-    document.body.style.backgroundColor = redcolor;
 
-}
+var stepOne = document.getElementById('stepOne');
+var stepOneSection = document.getElementById('stepOneSection');
+stepOne.onclick = function() {
+    stepOneSection.className = 'hidden';
 }
